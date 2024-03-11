d6d1b4b931b7b51fcd3e1aca3dde80ad38765ce9
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-11 22:40:55 +0200

    reverseFunc done

--- function-1.php
+++ function-1.php
@@ -1,7 +1,16 @@
 <?php
+// A function that flips the order of digits of a number.
 
-
-echo 'hello'
-
+function numFlipper ($num){
+    $revNum = 0;
+    while ($num > 1)  
+    {  
+    $rem = $num % 10;  
+    $revNum = ($revNum * 10) + $rem;  
+    $num = ($num / 10);   
+    } 
+    echo $revNum;
+}
+numFlipper(23456)
 
 ?>
